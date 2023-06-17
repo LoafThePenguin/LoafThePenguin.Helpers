3d18bafa1eb0b319ce4681c44a139ac856dd0195
Михаил Лоскутов <dev.toriki@gmail.com>
2023-06-18 00:14:14 +0500

    Добавление проекта

---
+++ src/LoafThePenguin.Helpers/DateOnlyHelper.cs
@@ -0,0 +1,49 @@
+namespace LoafThePenguin.Helpers;
+
+/// <summary>
+/// Содержит методы для работы с <see cref="DateOnly"/>
+/// </summary>
+public static class DateOnlyHelper
+{
+    /// <summary>
+    /// Возвращает текущую дату.
+    /// </summary>
+    public static DateOnly DateToday => DateOnly.FromDateTime(DateTime.Now);
+
+    /// <summary>
+    /// Ищет и возвращает следующую дату дня недели.
+    /// </summary>
+    /// <param name="dayOfWeek">День недели, дату которого нужно найти.</param>
+    /// <returns>Дата дня недели.</returns>
+    public static DateOnly NextDayOfWeekDate(DayOfWeek dayOfWeek)
+    {
+        return DayOfWeekDate(dayOfWeek, nextDayOfweek: true);
+    }
+
+    /// <summary>
+    /// Ищет и возвращает предыдущую дату дня недели.
+    /// </summary>
+    /// <param name="dayOfWeek">День недели, дату которого нужно найти.</param>
+    /// <returns>Дата дня недели.</returns>
+    public static DateOnly PrevDayOfWeekDate(DayOfWeek dayOfWeek)
+    {
+        return DayOfWeekDate(dayOfWeek, nextDayOfweek: false);
+    }
+
+    public static DateOnly Foo() => DateOnly.FromDateTime(DateTime.Now);
+
+    private static DateOnly DayOfWeekDate(DayOfWeek dayOfWeek, bool nextDayOfweek)
+    {
+        int direction = nextDayOfweek
+            ? 1
+            : -1;
+
+        DateOnly result = DateToday.AddDays(direction);
+        while(result.DayOfWeek != dayOfWeek)
+        {
+            result = DateToday.AddDays(direction);
+        }
+
+        return result;
+    }
+}
