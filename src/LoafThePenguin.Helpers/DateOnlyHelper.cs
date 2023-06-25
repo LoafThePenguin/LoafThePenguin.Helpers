1a405f5ba7f5c7f5b72fb2facf4c7e798e0086b6
LoafThePenguin <118125635+LoafThePenguin@users.noreply.github.com>
2023-06-26 01:38:25 +0500

    Revert "(feat) Добавлены методы вычисления следующего/предыдущего дня недели от конкретной даты."
    This reverts commit a20702bb97fee531763367ced1c141c0c3c9a0ce.

--- src/LoafThePenguin.Helpers/DateOnlyHelper.cs
+++ src/LoafThePenguin.Helpers/DateOnlyHelper.cs
@@ -11,54 +11,32 @@ public static class DateOnlyHelper
     public static DateOnly DateToday => DateOnly.FromDateTime(DateTime.Now);
 
     /// <summary>
-    /// Ищет и возвращает следующую дату дня недели от текущей даты.
+    /// Ищет и возвращает следующую дату дня недели.
     /// </summary>
-    /// <param name="nextDayOfWeek">День недели, дату которого нужно найти.</param>
+    /// <param name="dayOfWeek">День недели, дату которого нужно найти.</param>
     /// <returns>Дата дня недели.</returns>
-    public static DateOnly NextDayOfWeekDate(DayOfWeek nextDayOfWeek)
+    public static DateOnly NextDayOfWeekDate(DayOfWeek dayOfWeek)
     {
-        return DayOfWeekDate(DateToday, nextDayOfWeek, nextDayOfweek: true);
+        return DayOfWeekDate(dayOfWeek, nextDayOfweek: true);
     }
 
     /// <summary>
-    /// Ищет и возвращает следующую дату дня недели от даты <paramref name="fromDate"/>.
+    /// Ищет и возвращает предыдущую дату дня недели.
     /// </summary>
-    /// <param name="fromDate">Дата от которой нужно найти дату следующего дня недели <paramref name="nextDayOfWeek"/>.</param>
-    /// <param name="nextDayOfWeek">День недели, дату которого нужно найти.</param>
+    /// <param name="dayOfWeek">День недели, дату которого нужно найти.</param>
     /// <returns>Дата дня недели.</returns>
-    public static DateOnly NextDayOfWeekDateFrom(DateOnly fromDate, DayOfWeek nextDayOfWeek)
+    public static DateOnly PrevDayOfWeekDate(DayOfWeek dayOfWeek)
     {
-        return DayOfWeekDate(fromDate, nextDayOfWeek, nextDayOfweek: true);
+        return DayOfWeekDate(dayOfWeek, nextDayOfweek: false);
     }
 
-    /// <summary>
-    /// Ищет и возвращает предыдущую дату дня недели от текущей даты.
-    /// </summary>
-    /// <param name="prevDayOfWeek">День недели, дату которого нужно найти.</param>
-    /// <returns>Дата дня недели.</returns>
-    public static DateOnly PrevDayOfWeekDate(DayOfWeek prevDayOfWeek)
-    {
-        return DayOfWeekDate(DateToday, prevDayOfWeek, nextDayOfweek: false);
-    }
-
-    /// <summary>
-    /// Ищет и возвращает предыдущую дату дня недели от даты <paramref name="fromDate"/>..
-    /// </summary>
-    /// <param name="fromDate">Дата от которой нужно найти дату предыдущего дня недели <paramref name="prevDayOfWeek"/>.</param>
-    /// <param name="prevDayOfWeek">День недели, дату которого нужно найти.</param>
-    /// <returns>Дата дня недели.</returns>
-    public static DateOnly PrevDayOfWeekDateFrom(DateOnly fromDate, DayOfWeek prevDayOfWeek)
-    {
-        return DayOfWeekDate(fromDate, prevDayOfWeek, nextDayOfweek: false);
-    }
-
-    private static DateOnly DayOfWeekDate(DateOnly fromDate, DayOfWeek dayOfWeek, bool nextDayOfweek)
+    private static DateOnly DayOfWeekDate(DayOfWeek dayOfWeek, bool nextDayOfweek)
     {
         int direction = nextDayOfweek
             ? 1
             : -1;
 
-        DateOnly result = fromDate.AddDays(direction);
+        DateOnly result = DateToday.AddDays(direction);
         while(result.DayOfWeek != dayOfWeek)
         {
             result = result.AddDays(direction);
