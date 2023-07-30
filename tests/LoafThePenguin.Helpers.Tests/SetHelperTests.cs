57419c9a414292e6b14fdaec0cc93636295d44f2
Михаил Лоскутов <dev.toriki@gmail.com>
2023-07-30 18:26:17 +0500

    (wip) Восстановлен SetHelper.

---
+++ tests/LoafThePenguin.Helpers.Tests/SetHelperTests.cs
@@ -0,0 +1,146 @@
+﻿namespace LoafThePenguin.Helpers.Tests;
+
+public sealed class SetHelperTests
+{
+    private const int TIMEOUT = 1000;
+
+    [Theory(Timeout = TIMEOUT)]
+    [InlineData("abc", null)]
+    public void NullCheckSet_Throws_ANE_With_Objects<T>(T? field, T? value)
+    {
+        Assert.Throws<ArgumentNullException>(() => _ = SetHelper.NullCheckSet(ref field, value));
+    }
+
+    [Fact(Timeout = TIMEOUT)]
+    public void NullCheckSet_Throws_ANE_With_Nullable_Structs()
+    {
+        int? field = 0;
+        int? value = null;
+
+        Assert.Throws<ArgumentNullException>(() => _ = SetHelper.NullCheckSet(ref field, value));
+    }
+
+    [Fact(Timeout = TIMEOUT)]
+    public void NullCheckSet_Returns_False()
+    {
+        string field = "abc";
+        string value = "abc";
+
+        Assert.False(SetHelper.NullCheckSet(ref field, value));
+    }
+
+    [Fact(Timeout = TIMEOUT)]
+    public void NullCheckSet_Returns_True()
+    {
+        string field = "abc";
+        string value = "abcd";
+
+        Assert.True(SetHelper.NullCheckSet(ref field, value));
+    }
+
+    [Theory(Timeout = TIMEOUT)]
+    [InlineData(1, 1)]
+    [InlineData(1, 2)]
+    [InlineData(null, 2)]
+    [InlineData("abc", "abc")]
+    [InlineData("abc", "abcd")]
+    [InlineData(null, "abc")]
+    public void NullCheckSet_Sets_Value<T>(T? field, T? value)
+    {
+        SetHelper.NullCheckSet(ref field, value);
+
+        Assert.Equal(value, field);
+    }
+
+    [Fact(Timeout = TIMEOUT)]
+    public void NullCheckSet_Calls_Callback_When_Returns_True()
+    {
+        string field = "abc";
+        string value = "abcd";
+        int callbackVariable = 0;
+
+        SetHelper.NullCheckSet(ref field, value, () => ++callbackVariable);
+
+        Assert.Equal(1, callbackVariable);
+    }
+
+    [Fact(Timeout = TIMEOUT)]
+    public void NullCheckSet_Dont_Calls_Callback_When_Returns_False()
+    {
+        string field = "abc";
+        string value = "abc";
+        int callbackVariable = 0;
+
+        SetHelper.NullCheckSet(ref field, value, () => ++callbackVariable);
+
+        Assert.Equal(0, callbackVariable);
+    }
+
+    [Fact(Timeout = TIMEOUT)]
+    public void Set_Returns_False()
+    {
+        string field = "abc";
+        string value = "abc";
+
+        Assert.False(SetHelper.Set(ref field, value));
+    }
+
+    [Fact(Timeout = TIMEOUT)]
+    public void Set_Returns_True()
+    {
+        string field = "abc";
+        string value = "abcd";
+
+        Assert.True(SetHelper.Set(ref field, value));
+    }
+
+    [Theory(Timeout = TIMEOUT)]
+    [InlineData(1, 1)]
+    [InlineData(1, 2)]
+    [InlineData(null, 2)]
+    [InlineData("abc", "abc")]
+    [InlineData("abc", "abcd")]
+    [InlineData(null, "abc")]
+    [InlineData("abc", null)]
+    public void Set_Sets_Value<T>(T? field, T? value)
+    {
+        SetHelper.Set(ref field, value);
+
+        Assert.Equal(value, field);
+    }
+
+    [Fact(Timeout = TIMEOUT)]
+    public void Set_Sets_Value_With_Nullable_Structs()
+    {
+        int? field = 0;
+        int? value = null;
+
+        SetHelper.Set(ref field, value);
+
+        Assert.Equal(value, field);
+    }
+
+    [Fact(Timeout = TIMEOUT)]
+    public void Set_Calls_Callback_When_Returns_True()
+    {
+        string field = "abc";
+        string value = "abcd";
+        int callbackVariable = 0;
+
+        SetHelper.Set(ref field, value, () => ++callbackVariable);
+
+        Assert.Equal(1, callbackVariable);
+    }
+
+    [Fact(Timeout = TIMEOUT)]
+    public void Set_Dont_Calls_Callback_When_Returns_False()
+    {
+        string field = "abc";
+        string value = "abc";
+        int callbackVariable = 0;
+
+        SetHelper.Set(ref field, value, () => ++callbackVariable);
+
+        Assert.Equal(0, callbackVariable);
+    }
+}
