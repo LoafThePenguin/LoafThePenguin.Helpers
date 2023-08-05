c2fcbaec27757049b05a3f3fc625ccf2b97a5d35
Михаил Лоскутов <dev.toriki@gmail.com>
2023-08-05 18:31:52 +0500

    (feat) Добавлен интерфейс типа , способный читать ответ от Api.

---
+++ src/LoafThePenguin.ApiRequest/Abstracts/IApiResponseReader.cs
@@ -0,0 +1,21 @@
+namespace LoafThePenguin.MOEXSharp.ApiRequest.Abstracts;
+
+/// <summary>
+/// Представляет тип, способный читать ответ от Api.
+/// </summary>
+public interface IApiResponseReader
+{
+    /// <summary>
+    /// Асинхронно читает ответ от Api и возвращает объект с данными.
+    /// </summary>
+    /// <typeparam name="TResponse">
+    /// Тип экземпляра, который представляет данные ответа.
+    /// </typeparam>
+    /// <param name="response">
+    /// Экземпляр типа ответа от Api.
+    /// </param>
+    /// <returns>
+    /// Результат чтения ответа в представлении типа <typeparamref name="TResponse"/>.
+    /// </returns>
+    Task<TResponse?> ReadAsync<TResponse>(IApiResponse response);
+}
