79d728e5067d213934bbf6902709708a7c334835
Михаил Лоскутов <dev.toriki@gmail.com>
2023-08-05 18:24:21 +0500

    (feat) Добавлен интерфейс запроса к Api - IApiRequestMessage.

---
+++ src/LoafThePenguin.ApiRequest/Abstracts/IApiRequestMessage.cs
@@ -0,0 +1,29 @@
+namespace LoafThePenguin.MOEXSharp.ApiRequest.Abstracts;
+
+/// <summary>
+/// Представляет тип запроса к Api.
+/// </summary>
+public interface IApiRequestMessage : IDisposable
+{
+    /// <summary>
+    /// Конфигурирует заголовки запроса.
+    /// </summary>
+    /// <param name="name">Название заголовка.</param>
+    /// <param name="values">Перечисление значений заголовка.</param>
+    /// <returns>Экземпляр запроса к Api.</returns>
+    IApiRequestMessage ConfigureHeaders(string name, IEnumerable<string> values);
+
+    /// <summary>
+    /// Конфигурирует заголовки запроса.
+    /// </summary>
+    /// <param name="name">Название заголовка.</param>
+    /// <param name="value">Значение заголовка.</param>
+    /// <returns>Экземпляр запроса к Api.</returns>
+    IApiRequestMessage ConfigureHeaders(string name, string value);
+
+    /// <summary>
+    /// Асинхронно отправляет запрос к Api.
+    /// </summary>
+    /// <returns>Экземпляр ответа на запрос от Api.</returns>
+    Task<IApiResponse> SendAsync();
+}
