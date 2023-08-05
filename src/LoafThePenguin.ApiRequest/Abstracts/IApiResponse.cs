2e4498028e3823e0687a66d90c3d993a9c2c53ad
Михаил Лоскутов <dev.toriki@gmail.com>
2023-08-05 18:18:39 +0500

    (feat) Добавлен интерфейс IApiResponse.
    - IApiResponse упрощённая обёртка над ответом от Api.

---
+++ src/LoafThePenguin.ApiRequest/Abstracts/IApiResponse.cs
@@ -0,0 +1,23 @@
+namespace LoafThePenguin.MOEXSharp.ApiRequest.Abstracts;
+
+/// <summary>
+/// Представляет тип ответа Api.
+/// </summary>
+public interface IApiResponse: IDisposable, IAsyncDisposable
+{
+    /// <summary>
+    /// Возвращает статус-код ответа.
+    /// </summary>
+    int StatusCode { get; }
+
+    /// <summary>
+    /// Возвращает <see langword="true"/>, если запрос был отправлен успешно и ответ был получен. 
+    /// Иначе - возвращает <see langword="false"/>.
+    /// </summary>
+    bool IsSuccess { get; }
+
+    /// <summary>
+    /// Возвращает поток с ответом на запрос.
+    /// </summary>
+    Stream ResponseStream { get; }
+}
