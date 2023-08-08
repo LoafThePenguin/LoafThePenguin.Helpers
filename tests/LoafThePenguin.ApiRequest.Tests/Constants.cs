da736ef143764c3f873192c0389ab77ac7916ac6
Михаил Лоскутов <dev.toriki@gmail.com>
2023-08-08 13:15:33 +0500

    (feat) Добавлен класс, способный читать ответ в формате json.

--- tests/LoafThePenguin.ApiRequest.Tests/Constants.cs
+++ tests/LoafThePenguin.ApiRequest.Tests/Constants.cs
@@ -2,5 +2,6 @@ namespace LoafThePenguin.ApiRequest.Tests;
 
 public static class Constants
 {
+    public const int TIMEOUT_1_SEC = 1000;
     public const string LOCALHOST_TEST_URI = "https://www.localhost.me/";
 }
